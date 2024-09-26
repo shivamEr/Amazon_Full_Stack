265617376ebcb5def16652cc1aac9f9174a3a3dc
shivam kumar <shivamksbabu504@gmail.com>
2024-09-26 13:47:54 +0530

    Set Route and Home Page Ready

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,18 +1,20 @@
 
 import Navbar from './Components/Navbar/Navbar';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import Home from './Pages/Home';
+
+import Shop from './Pages/Shop';
 import ShopCategory from './Pages/ShopCategory';
 import Product from './Pages/Product';
 import Cart from './Pages/Cart';
 import LoginSign from './Pages/LoginSign';
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
       <Navbar/>
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Shop/>}/>
         <Route path='/mens' element={<ShopCategory category="men"/>}/>
         <Route path='/womens' element={<ShopCategory category="women"/>}/>
         <Route path='/kids' element={<ShopCategory category="kid"/>}/>
@@ -21,11 +23,6 @@ function App() {
         </Route>
         <Route path='/cart' element={<Cart/>}/>
         <Route path='/login' element={<LoginSign/>}/>
-        <Route path='/signin' element={<LoginSign/>}/>
-
-        
-
-
       </Routes>
       </BrowserRouter>
     </div>
