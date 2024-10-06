e690c06376ebaa3cea62bd8b5f4aa61d8297230c
shivam kumar <shivamksbabu504@gmail.com>
2024-10-06 15:45:47 +0530

    Shop Category Page Builded

--- FrontEnd/src/Pages/ShopCategory.jsx
+++ FrontEnd/src/Pages/ShopCategory.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import './CSS/ShopCategory.css'
+import { ShopContext } from '../Context/ShopContext'
+import dropdown_icon from '../Components/Assets/dropdown_icon.png'
+import Item from '../Components/Items/Item'
 
-const ShopCategory = () => {
+const ShopCategory = (props) => {
+  const { all_product } = useContext(ShopContext);
   return (
-    <div>
-      <h1>Shop Categorys</h1>
+    <div className='shop-category'>
+      <img src={props.banner} alt="" className='shopcategory-banner' />
+      <div className="shopcategory-indexSort">
+        <p>
+          <span>Showing 1-12</span> Out of 36 Products
+        </p>
+        <div className="shopcategory-sort">
+          Sort by <img src={dropdown_icon} alt="" />
+        </div>
+      </div>
+      <div className="shopcategory-products">
+        {all_product.map((item, index) => {
+          if (props.category === item.category) {
+            return <Item key={index} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+          }
+          else return null;
+        })}
+      </div>
+      <div className="shopcategory-loadmore">
+        Explore More
+      </div>
+
     </div>
   )
 }
